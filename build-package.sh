3aca965ed436a4df7282b5aa9360dabdee1463e3
Fernandez Ludovic <lfernandez.dev@gmail.com>
2017-05-27 18:14:17 +0200

    feat: Speed up Docker build.

--- build-package.sh
+++ build-package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:symless/synergy.git
+git clone --depth=10 git@github.com:symless/synergy.git
 
 ## Build Ubuntu version
 docker build -t synergy-ubuntu -f ubuntu/Dockerfile .
@@ -9,7 +9,7 @@ mkdir -p build-ubuntu
 docker run --rm -v "$PWD/build-ubuntu":'/src/synergy/bin' synergy-ubuntu ./build.sh
 
 ## Build Centos version
-docker build -t synergy-centos -f centos/Dockerfile .
-rm -rf build-centos
-mkdir -p build-centos
-docker run --rm -v "$PWD/build-centos":'/src/synergy/bin' synergy-centos ./build.sh
+# docker build -t synergy-centos -f centos/Dockerfile .
+# rm -rf build-centos
+# mkdir -p build-centos
+# docker run --rm -v "$PWD/build-centos":'/src/synergy/bin' synergy-centos ./build.sh
